ae274ddfdce0f33a4576efd72509cb8475e1df27
Vsparker <Victor.parkx@gmail.com>
2021-01-30 15:45:18 -0500

    setup cartproduct model and action creator to fetch and set user cart products

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,8 @@ import {
 
 import {
     fetchUserPurchases,
-    setPurchaseDetail
+    setPurchaseDetail,
+    fetchCartProducts
 } from './user';
 
 import {
--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import CartProduct from './cartProduct';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
@@ -8,9 +11,10 @@ function CartButton({className, icon}) {
         </div>
     )
 }
+
 function CartContent({className, products}) {
     let count = products.length;
-    let productsJSX = products.map(product => <CartProduct key={product}/>);
+    let productsJSX = products.map(product => <CartProduct key={product._id}/>);
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
@@ -23,7 +27,6 @@ function CartContent({className, products}) {
         </div>
     )
 }
-
 function CartFooter({className, products}) {
     const price = 7.96;
     return (
@@ -40,14 +43,31 @@ function CartFooter({className, products}) {
         </div>
     )
 }
+
 class ShopCart extends Component {
+
+    componentDidMount() {
+        this.props.fetchCartProducts();
+    }
+
     render() {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554, 243, 3434, 4554, 3434, 4554, 243, 3434, 4554]}/>            </div>
+                <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
+            </div>
         )
     }
 }
-export default ShopCart;
+
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return {
+        cartProducts
+    }
+}
+
+ShopCart = connect(mapStateToProps, actions)(ShopCart);
+
+export default ShopCart; 
