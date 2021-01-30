5fccab920766be781b5e8f0ee18208277c249303
Vsparker <Victor.parkx@gmail.com>
2021-01-30 16:34:02 -0500

    cart animations and cart button in shop component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -3,8 +3,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
+import CartButton from './cartButton';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            showCart: true
+        }
+    }
     componentDidMount() {
         const headerLinks = [
             {
@@ -15,21 +24,25 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
-
         // filter products with links
         this.props.fetchShopProducts();
     }
-
     shouldComponentUpdate(nextProps) {
         if(this.props != nextProps) {
             this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
         }
         return true
-
+    }
+    onSubmit = (fields) => {
+        this.props.filterProductsWithQuery(fields)
     }
 
-    onSubmit = (fields) => {
-        console.log(fields);
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
     }
 
 
@@ -47,7 +60,7 @@ class Shop extends Component {
                         })
                     }
                 </div>
-                {/* shop cart button */}
+                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
             </div>
         )
     }
