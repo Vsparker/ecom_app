0cd7a6e403c15119ea4a8df1993b221b022d117a
Vsparker <Victor.parkx@gmail.com>
2021-01-31 14:28:44 -0500

    setup order review component and added page title component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from './reducers';
-
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
-import "./style/main.scss";
-
+import './style/main.scss';
 import history from './history';
-
-import Layout from "./components/layout";
+import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Account from './components/account/account';
 import Shop from './components/shop/shop';
+import Review from './components/order/review';
 
 function main() {
   ReactDOM.render(
@@ -22,21 +20,17 @@ function main() {
       <Router history={history}>
         <Layout>
           <Switch>
-
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' exact component={Signin}/>
             <Route path='/signup' exact component={Signup}/>
-
             <Route path='/account' exact component={Account}/>
 
             <Route path='/shop' exact component={Shop}/>
-
+            <Route path='/order/review' exact component={Review}/>
           </Switch>
         </Layout>
       </Router>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
-
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
